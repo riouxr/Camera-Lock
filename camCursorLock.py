a4d75ad5187b0e0604d646bd3cc8771a7b5b9d13
riouxr <riouxr@gmail.com>
2025-09-11 08:43:55 -0400

    Update camCursorLock.py

--- camCursorLock.py
+++ camCursorLock.py
@@ -23,7 +23,7 @@ bl_info = {
     "blender": (2, 93, 0),
     "location": "View3D > Header",
     "description": "Toggle lock camera and/or cursor",
-    "category": "Object"
+    "category": "Object",
     "support": "COMMUNITY"      
 }
 
